9bf6cc3ad224a4882aad948ab5766338fc0bb5e9
brycebohling <96216764+brycebohling@users.noreply.github.com>
2023-05-07 17:10:24 -0700

    jump working

--- Assets/Scripts/PlayerC.cs
+++ Assets/Scripts/PlayerC.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class PlayerC : MonoBehaviour
 {
     [Header("Movement")]
-    Rigidbody rb;
     [SerializeField] float moveSpeed;
     [SerializeField] Transform orientation;
+    [SerializeField] float groundDrag;
+    [SerializeField] float jumpForce;
+    [SerializeField] float jumpCooldown;
+    [SerializeField] float airMultiplier;
+    bool readyToJump = true;
+
+    [Header("Ground Check")]
+    [SerializeField] float playerHeight;
+    [SerializeField] LayerMask groundLayer;
+    bool isGrounded;
+
+    [Header("Keybinds")]
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+
+    Rigidbody rb;
     float horizontalInput;
     float verticalInput;
     Vector3 moveDirection;
@@ -21,25 +35,82 @@ public class PlayerC : MonoBehaviour
     
     void Update()
     {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.05f, groundLayer);
+
         GetInputs();
         
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        } else
+        {
+            rb.drag = 0f;
+        }
     }
 
     void FixedUpdate() 
     {
         MovePlayer();
+        SpeedControl();
     }
 
     void GetInputs()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(jumpKey) && readyToJump && isGrounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
 
     void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
+        if (isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
+        } else if (!isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier, ForceMode.Force);
+        }
+
+        transform.rotation = orientation.rotation;
+    }
+
+    void SpeedControl()
+    {
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if (flatVelocity.magnitude > moveSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+        }
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+
+
+    }
+
+    void ResetJump()
+    {
+        readyToJump = true; 
+    }
+
+    private void OnDrawGizmos() 
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y -0.05f, transform.position.z));    
     }
 }
